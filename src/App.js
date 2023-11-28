8220ce2dba1d70d6e0b0bec0fcbbbe0527aff896
syeef <syeefislam@yahoo.com>
2023-11-28 08:19:29 +0530

    list console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const data = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description: "The best tiramisu in town",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$5.00",
+  },
+  {
+    id: "2",
+    title: "Lemon Ice Cream",
+    description: "Mind blowing taste",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$6.00",
+  },
+  {
+    id: "3",
+    title: "Chocolate mousse",
+    description: "Unexplored flavour",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$7.00",
+  },
+];
+
+const topDesserts = data.map((dessert) => {
+  return {
+    content: `${dessert.title} - ${dessert.description}`,
+    price: dessert.price,
+  };
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(topDesserts);
+  return <h1>Little Lemon</h1>;
 }
 
 export default App;
