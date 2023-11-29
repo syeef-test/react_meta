879513046cd9ffb12144383824484b069402a997
syeef <syeefislam@yahoo.com>
2023-11-29 18:51:36 +0530

    basic form created

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import "./App.css";
 import { useState } from "react";
 
-const ToDo = (props) => (
-  <tr>
-    <td>
-      <label>{props.id}</label>
-    </td>
-    <td>
-      <input />
-    </td>
-    <td>
-      <label>{props.createdAt}</label>
-    </td>
-  </tr>
-);
-
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: "todo1",
-      createdAt: "18.00",
-    },
-    {
-      id: "todo2",
-      createdAt: "20.00",
-    },
-  ]);
-
-  const reverseOrder = () => {
-    //Reverse is a mutative operation, so we need to create a new array first
-    setTodos([...todos].reverse());
-  };
-
-  //First example with keys,show browser console to see warning
   return (
-    <div>
-      <button onClick={reverseOrder}>Reverse</button>
-      <table>
-        <tbody>
-          {todos.map((todo, index) => (
-            <ToDo key={todo.id} id={todo.id} createdAt={todo.createdAt} />
-          ))}
-        </tbody>
-      </table>
+    <div className="App">
+      <form>
+        <fieldset>
+          <div className="Field">
+            <label>Name:</label>
+            <input type="text" placeholder="Name" name="name" />
+          </div>
+          <button type="submit">Submit</button>
+        </fieldset>
+      </form>
     </div>
   );
 }
