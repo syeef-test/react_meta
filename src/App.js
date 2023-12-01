724c76c79586baec40e552eb35a26c26441cce27
syeef <syeefislam@yahoo.com>
2023-12-01 07:08:36 +0530

    basic useState example

--- src/App.js
+++ src/App.js
@@ -1,51 +1,18 @@
 import "./App.css";
-// import { useState } from "react";
-import { UserProvider, useUser } from "./UserContext";
-
-const LoggedInUser = () => {
-  const { user } = useUser();
-  return (
-    <p>
-      Hello <span className="Username">{user.name}</span>
-    </p>
-  );
-};
-
-const Header = () => {
-  return (
-    <header>
-      <h2>Blog App</h2>
-      <LoggedInUser />
-    </header>
-  );
-};
-
-const Page = () => {
-  const { user } = useUser();
-  return (
-    <div>
-      <h2>What is lorem</h2>
-      <p>aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaa</p>
-      <p>Written By: {user.name}</p>
-    </div>
-  );
-};
+import { useState } from "react";
 
 function App() {
+  const [restaurentName, setResturentName] = useState("Lemon");
+
+  function updateResturentName() {
+    setResturentName("Little Lemon");
+  }
   return (
-    <div className="App">
-      <Header />
-      <Page />
+    <div>
+      <h1>{restaurentName}</h1>
+      <button onClick={updateResturentName}>Update Resturent Name</button>
     </div>
   );
 }
 
-function Root() {
-  return (
-    <UserProvider>
-      <App />
-    </UserProvider>
-  );
-}
-
-export default Root;
+export default App;
