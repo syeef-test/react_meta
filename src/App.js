7c989cb609b9184e4518c965a0cd51e8d8ac5c86
syeef <syeefislam@yahoo.com>
2023-12-01 20:09:58 +0530

    normal ex of useEffect

--- src/App.js
+++ src/App.js
@@ -1,69 +1,24 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import { useState } from "react";
-import React from "react";
-
-function GoalForm(props) {
-  const [formData, setFormData] = useState({ goal: "", by: "" });
-
-  function changeHandler(e) {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  }
-
-  function submitHandler(e) {
-    e.preventDefault();
-    props.onAdd(formData);
-    setFormData({ goal: "", by: "" });
-  }
-
-  return (
-    <>
-      <h1>My Little Lemon Goals</h1>
-      <form onSubmit={submitHandler}>
-        <input
-          type="text"
-          name="goal"
-          placeholder="Goal"
-          value={formData.goal}
-          onChange={changeHandler}
-        />
-        <input
-          type="text"
-          name="by"
-          placeholder="By"
-          value={formData.by}
-          onChange={changeHandler}
-        />
-        <button>Submit Goal</button>
-      </form>
-    </>
-  );
-}
-
-function ListOfGoals(props) {
-  return (
-    <ul>
-      {props.allGoals.map((g) => (
-        <li key={g.goal}>
-          <span>
-            My goal is to {g.goal},by {g.by}
-          </span>
-        </li>
-      ))}
-    </ul>
-  );
-}
 
 function App() {
-  const [allGoals, updateAllGoals] = React.useState([]);
+  const [toggle, setToggle] = useState(false);
 
-  function addGoal(goal) {
-    updateAllGoals([...allGoals, goal]);
-  }
+  const clickHandler = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    document.title = toggle
+      ? "Welcome to Little Lemon"
+      : "Using the useEffect hook";
+  }, [toggle]);
 
   return (
     <div>
-      <GoalForm onAdd={addGoal} />
-      <ListOfGoals allGoals={allGoals} />
+      <h1>Using the useEffect Hook</h1>
+      <button onClick={clickHandler}>Toggle Message</button>
+      {toggle && <h2>Welcome to little lemon</h2>}
     </div>
   );
 }
