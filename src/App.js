7305848ec2082e7ef015b3edbd5c51ba5a310b4c
syeef <syeefislam@yahoo.com>
2023-12-01 10:18:21 +0530

    goal app done by useState and prop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,69 @@
 import "./App.css";
 import { useState } from "react";
+import React from "react";
+
+function GoalForm(props) {
+  const [formData, setFormData] = useState({ goal: "", by: "" });
+
+  function changeHandler(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: "", by: "" });
+  }
+
+  return (
+    <>
+      <h1>My Little Lemon Goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="Goal"
+          value={formData.goal}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="By"
+          value={formData.by}
+          onChange={changeHandler}
+        />
+        <button>Submit Goal</button>
+      </form>
+    </>
+  );
+}
+
+function ListOfGoals(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal},by {g.by}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
-  const [restaurentName, setResturentName] = useState("Lemon");
+  const [allGoals, updateAllGoals] = React.useState([]);
 
-  function updateResturentName() {
-    setResturentName("Little Lemon");
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal]);
   }
+
   return (
     <div>
-      <h1>{restaurentName}</h1>
-      <button onClick={updateResturentName}>Update Resturent Name</button>
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
     </div>
   );
 }
