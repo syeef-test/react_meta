23b48068cae1a45302b4fba798527ec6d0dd60d2
syeef <syeefislam@yahoo.com>
2023-12-02 18:30:35 +0530

    how to use uesRef to hook into DOM element

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React, { useReducer } from "react";
+import React from "react";
 import "./App.css";
 
-const reducer = (state, action) => {
-  if (action.type === "buy_ingredients") return { money: state.money - 10 };
-  if (action.type === "sell_a_meal") return { money: state.money + 10 };
-  if (action.type === "celebrity_visit") return { money: state.money + 5000 };
-  return state;
-};
-
 function App() {
-  const initialState = { money: 100 };
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const formInputRef = React.useRef(null);
+
+  const focusInput = () => {
+    formInputRef.current.focus();
+  };
 
   return (
-    <div className="App">
-      <h1>Wallet:{state.money}</h1>
-      <div>
-        <button onClick={() => dispatch({ type: "buy_ingredients" })}>
-          Shopping for vegies!
-        </button>
-        <button onClick={() => dispatch({ type: "sell_a_meal" })}>
-          Serve a meal to the customer
-        </button>
-        <button onClick={() => dispatch({ type: "celebrity_visit" })}>
-          Celebrity visit
-        </button>
-      </div>
-    </div>
+    <>
+      <h1>Using useRef to access underlying DOM</h1>
+      <input ref={formInputRef} type="text" />
+      <button onClick={focusInput}>Focus Input</button>
+    </>
   );
 }
 
