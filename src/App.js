030416bcbc3c09f563cf87f76e327ba153d54864
syeef <syeefislam@yahoo.com>
2023-12-03 19:24:36 +0530

    jest and react test library automation test simple example

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const DataFetcher = ({ render, url }) => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    if (url.includes("desserts")) {
-      setData(["cake", "ice cream", "pie", "brownie"]);
-    } else {
-      setData(["water", "soda", "juice"]);
-    }
-  }, [url]);
-
-  return render(data);
-};
-
-const DessertsCount = () => {
-  return (
-    <DataFetcher
-      url="https://littlelemon/desserts"
-      render={(data) => <p>{data.length} desserts</p>}
-    />
-  );
-};
-
-const DrinksCount = () => {
-  return (
-    <DataFetcher
-      url="https://littlelemon/drinks"
-      render={(data) => <p>{data.length} drinks</p>}
-    />
-  );
-};
-
 function App() {
   return (
     <div className="App">
-      <header className="Header">Little Lemon Restaurant</header>
-      <DessertsCount />
-      <DrinksCount />
+      <a href="https://littlelemon.com" className="App-link">
+        Little Lemon Resturent
+      </a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders a link that points to Little Lemon webpage", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Little Lemon Resturent");
   expect(linkElement).toBeInTheDocument();
 });
